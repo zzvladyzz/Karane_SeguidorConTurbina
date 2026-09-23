5109e8ba25a656e4223b18604aaadb9391f56aee
zzvladyzz <vladimir_1913@hotmail.com>
2026-09-23 22:48:01 +0200

    se corrigio ultimo modelo

--- Inda_STM/Core/Inc/main.h
+++ Inda_STM/Core/Inc/main.h
@@ -57,66 +57,14 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define AMP_ML_Pin GPIO_PIN_0
-#define AMP_ML_GPIO_Port GPIOC
-#define AMP_MR_Pin GPIO_PIN_1
-#define AMP_MR_GPIO_Port GPIOC
-#define VOLT_BATT_Pin GPIO_PIN_2
-#define VOLT_BATT_GPIO_Port GPIOC
-#define IN_MUX_Pin GPIO_PIN_3
-#define IN_MUX_GPIO_Port GPIOC
-#define Encoder_A_ML_Pin GPIO_PIN_0
-#define Encoder_A_ML_GPIO_Port GPIOA
-#define Encoder_B_ML_Pin GPIO_PIN_1
-#define Encoder_B_ML_GPIO_Port GPIOA
-#define Encoder_C_ML_Pin GPIO_PIN_2
-#define Encoder_C_ML_GPIO_Port GPIOA
-#define Encoder_A_MR_Pin GPIO_PIN_5
-#define Encoder_A_MR_GPIO_Port GPIOA
-#define S0_MUX_Pin GPIO_PIN_4
-#define S0_MUX_GPIO_Port GPIOC
-#define S1_MUX_Pin GPIO_PIN_5
-#define S1_MUX_GPIO_Port GPIOC
-#define S2_MUX_Pin GPIO_PIN_0
-#define S2_MUX_GPIO_Port GPIOB
-#define S3_MUX_Pin GPIO_PIN_1
-#define S3_MUX_GPIO_Port GPIOB
-#define BOTON_START_Pin GPIO_PIN_2
-#define BOTON_START_GPIO_Port GPIOB
-#define BOTON_STOP_Pin GPIO_PIN_10
-#define BOTON_STOP_GPIO_Port GPIOB
-#define LED_4_Pin GPIO_PIN_12
-#define LED_4_GPIO_Port GPIOB
-#define LED_3_Pin GPIO_PIN_13
-#define LED_3_GPIO_Port GPIOB
-#define LED_2_Pin GPIO_PIN_14
-#define LED_2_GPIO_Port GPIOB
-#define LED_1_Pin GPIO_PIN_15
-#define LED_1_GPIO_Port GPIOB
-#define LED_ALARMA_Pin GPIO_PIN_8
-#define LED_ALARMA_GPIO_Port GPIOC
-#define LED_OK_Pin GPIO_PIN_9
+#define LED_OK_Pin GPIO_PIN_13
 #define LED_OK_GPIO_Port GPIOC
-#define PWM_FAN_Pin GPIO_PIN_8
-#define PWM_FAN_GPIO_Port GPIOA
-#define PWM_RED_Pin GPIO_PIN_9
-#define PWM_RED_GPIO_Port GPIOA
-#define PWM_GREEN_Pin GPIO_PIN_10
-#define PWM_GREEN_GPIO_Port GPIOA
-#define PWM_BLUE_Pin GPIO_PIN_11
-#define PWM_BLUE_GPIO_Port GPIOA
-#define IR_36KHZ_Pin GPIO_PIN_12
-#define IR_36KHZ_GPIO_Port GPIOA
-#define SPI3_NSS_Pin GPIO_PIN_15
-#define SPI3_NSS_GPIO_Port GPIOA
-#define Encoder_C_MR_Pin GPIO_PIN_2
-#define Encoder_C_MR_GPIO_Port GPIOD
-#define Encoder_B_MR_Pin GPIO_PIN_3
-#define Encoder_B_MR_GPIO_Port GPIOB
-#define EN_SENSORES_Pin GPIO_PIN_4
-#define EN_SENSORES_GPIO_Port GPIOB
-#define EN_MOTOR_Pin GPIO_PIN_5
-#define EN_MOTOR_GPIO_Port GPIOB
+#define LED_ALARMA_Pin GPIO_PIN_14
+#define LED_ALARMA_GPIO_Port GPIOC
+#define LED_AVISO_Pin GPIO_PIN_15
+#define LED_AVISO_GPIO_Port GPIOC
+#define SPI_NSS_Pin GPIO_PIN_15
+#define SPI_NSS_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
--- Inda_STM/Core/Src/main.c
+++ Inda_STM/Core/Src/main.c
@@ -18,20 +18,18 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "adc.h"
-#include "spi.h"
-#include "tim.h"
-#include "usart.h"
-#include "gpio.h"
-
+#include "stdio.h"
+#include "string.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "LIB_MPU6500_SPI.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
-
+MPU6500_Init_Values_t 	MPU6500_Raw;
+MPU6500_Init_float_t	MPU6500_Conv;
+MPU6500_status_e	MPU6500_Status;
 /* USER CODE END PTD */
 
 /* Private define ------------------------------------------------------------*/
@@ -45,6 +43,9 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+SPI_HandleTypeDef hspi3;
+char bufferTxt[30];
+UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
 
@@ -52,6 +53,9 @@
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_SPI3_Init(void);
+static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -90,31 +94,48 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_ADC1_Init();
   MX_SPI3_Init();
-  MX_TIM1_Init();
-  MX_TIM2_Init();
-  MX_TIM4_Init();
-  MX_TIM5_Init();
-  MX_USART6_UART_Init();
+  MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
-
+  MPU6500_Status=MPU6500_Init(&MPU6500_Raw,50,DPS250,G2);
+  if (MPU6500_Status==MPU6500_fail) {
+	  for (;;) {
+		  sprintf(bufferTxt," Fallo al iniciar MPU\r\n ");
+		  HAL_UART_Transmit(&huart1, (uint8_t *)bufferTxt, strlen(bufferTxt), HAL_MAX_DELAY);
+		  HAL_Delay(1000);
+	  }
+  }
+  sprintf(bufferTxt,"Exito al iniciar MPU\r\n ");
+  HAL_UART_Transmit(&huart1, (uint8_t *)bufferTxt, strlen(bufferTxt), HAL_MAX_DELAY);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-	  HAL_GPIO_TogglePin(LED_ALARMA_GPIO_Port, LED_ALARMA_Pin);
-	  HAL_GPIO_TogglePin(LED_OK_GPIO_Port, LED_OK_Pin);
-	  HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
-	  HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);
-	  HAL_GPIO_TogglePin(LED_3_GPIO_Port, LED_3_Pin);
-	  HAL_GPIO_TogglePin(LED_4_GPIO_Port, LED_4_Pin);
 
+	  	 LED_OK_GPIO_Port->ODR|=LED_OK_Pin;
+	  		  HAL_Delay(300);
+
+	  LED_ALARMA_GPIO_Port->ODR|=LED_ALARMA_Pin;
+	  HAL_Delay(300);
+
+	  LED_AVISO_GPIO_Port->ODR|=LED_AVISO_Pin;
+	  HAL_Delay(300);
+
+	  LED_OK_GPIO_Port->ODR&=~LED_OK_Pin;
+	  LED_AVISO_GPIO_Port->ODR&=~LED_AVISO_Pin;
+	  LED_ALARMA_GPIO_Port->ODR&=~LED_ALARMA_Pin;
 	  HAL_Delay(1000);
 
-
+	  MPU6500_Read(&MPU6500_Raw);
+	  		MPU6500_Conv=MPU6500_Converter(&MPU6500_Raw);
+	  		sprintf(bufferTxt," ax= %0.2f ",MPU6500_Conv.MPU6500_floatAX);
+	  		HAL_UART_Transmit(&huart1, (uint8_t *)bufferTxt, strlen(bufferTxt), HAL_MAX_DELAY);
+	  		sprintf(bufferTxt," ay= %0.2f ",MPU6500_Conv.MPU6500_floatAY);
+	  		HAL_UART_Transmit(&huart1, (uint8_t *)bufferTxt, strlen(bufferTxt), HAL_MAX_DELAY);
+	  		sprintf(bufferTxt," az= %0.2f \r\n",MPU6500_Conv.MPU6500_floatAZ);
+	  		HAL_UART_Transmit(&huart1, (uint8_t *)bufferTxt, strlen(bufferTxt), HAL_MAX_DELAY);
 
     /* USER CODE END WHILE */
 
@@ -135,7 +156,7 @@ void SystemClock_Config(void)
   /** Configure the main internal regulator output voltage
   */
   __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -145,7 +166,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLN = 168;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 4;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
@@ -159,13 +180,126 @@ void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief SPI3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_SPI3_Init(void)
+{
+
+  /* USER CODE BEGIN SPI3_Init 0 */
+
+  /* USER CODE END SPI3_Init 0 */
+
+  /* USER CODE BEGIN SPI3_Init 1 */
+
+  /* USER CODE END SPI3_Init 1 */
+  /* SPI3 parameter configuration*/
+  hspi3.Instance = SPI3;
+  hspi3.Init.Mode = SPI_MODE_MASTER;
+  hspi3.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi3.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi3.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi3.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi3.Init.NSS = SPI_NSS_SOFT;
+  hspi3.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
+  hspi3.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi3.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi3.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi3.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN SPI3_Init 2 */
+
+  /* USER CODE END SPI3_Init 2 */
+
+}
+
+/**
+  * @brief USART1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART1_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART1_Init 0 */
+
+  /* USER CODE END USART1_Init 0 */
+
+  /* USER CODE BEGIN USART1_Init 1 */
+
+  /* USER CODE END USART1_Init 1 */
+  huart1.Instance = USART1;
+  huart1.Init.BaudRate = 115200;
+  huart1.Init.WordLength = UART_WORDLENGTH_8B;
+  huart1.Init.StopBits = UART_STOPBITS_1;
+  huart1.Init.Parity = UART_PARITY_NONE;
+  huart1.Init.Mode = UART_MODE_TX_RX;
+  huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART1_Init 2 */
+
+  /* USER CODE END USART1_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+
+  /* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, LED_OK_Pin|LED_ALARMA_Pin|LED_AVISO_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(SPI_NSS_GPIO_Port, SPI_NSS_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED_OK_Pin LED_ALARMA_Pin LED_AVISO_Pin */
+  GPIO_InitStruct.Pin = LED_OK_Pin|LED_ALARMA_Pin|LED_AVISO_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : SPI_NSS_Pin */
+  GPIO_InitStruct.Pin = SPI_NSS_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(SPI_NSS_GPIO_Port, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
